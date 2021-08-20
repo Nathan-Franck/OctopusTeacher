dc174f070beeb657a247ffdc1501ee3e8059323a
TacticalDan <gorksorf@gmail.com>
2021-08-20 16:07:37 -0600

    More new helper functions for ECS

--- OctopusTeacher/utils.h
+++ OctopusTeacher/utils.h
@@ -29,6 +29,41 @@ T* componentFromEntity(Entity ent) {
 	return GetScene().GetManager<T>()->GetComponent(ent);
 }
 
+bool isAncestorOfEntity(Entity entity, Entity ancestor) {
+	auto heirarchyComponent = GetScene().hierarchy.GetComponent(ancestor);
+	if (heirarchyComponent == nullptr) { return false; }
+	Entity parent = heirarchyComponent->parentID;
+	while (parent != wiECS::INVALID_ENTITY)
+	{
+		if (entity == parent) { return true; }
+		ancestor = parent;
+		auto heirarchyComponent = GetScene().hierarchy.GetComponent(ancestor);
+		if (heirarchyComponent == nullptr) { return false; }
+		parent = heirarchyComponent->parentID;
+	}
+	return false;
+}
+
+Entity findOffspringWithName(Entity entity, string name) {
+	auto manager = GetScene().GetManager<NameComponent>();
+	for (int i = 0; i < manager->GetCount(); i++) {
+		auto ent = manager->GetEntity(i);
+		if (!isAncestorOfEntity(entity, ent)) { continue; }
+		auto nameComponent = (*manager)[i];
+		if (nameComponent.name.compare(name) == 0) { return ent;  }
+	}
+	return wiECS::INVALID_ENTITY;
+}
+
+Entity findWithName(string name) {
+	auto manager = GetScene().GetManager<NameComponent>();
+	for (int i = 0; i < manager->GetCount(); i++) {
+		auto nameComponent = (*manager)[i];
+		if (nameComponent.name.compare(name) == 0) { return manager->GetEntity(i); }
+	}
+	return wiECS::INVALID_ENTITY;
+}
+
 vector<vector<Entity>> getLimbsForOctopusScene(Entity octopusScene)
 {
 	auto isOctopus = [](Entity entity)
