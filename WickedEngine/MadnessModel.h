00268245deb2883e7068e2dd55f12226fe8a46d1
TacticalDan <gorksorf@gmail.com>
2021-09-25 20:45:43 -0600

    MadnessModel actually triggering responders! Response order is a function of submit tuple's type order

--- WickedEngine/MadnessModel.h
+++ WickedEngine/MadnessModel.h
@@ -1,34 +1,116 @@
 #pragma once
 #include <type_traits>
 
+template <typename T> struct InspectLambda : InspectLambda<decltype(&T::operator())> {};
+template <typename Out, typename C, typename In>
+struct InspectLambda<Out(C::*)(In) const>
+{
+	using Input = In;
+	using Output = Out;
+};
+
+template <typename T> struct TupleHelper : InspectLambda<T> {};
+template <typename... Member>
+struct TupleHelper <tuple<Member...>>
+{
+	bool hasIndex(type_index index)
+	{
+		return ((typeid(Member) == index) || ...);
+	}
+};
+ 
 template<class... Responder>
 class MadnessModel {
-	using Inputs = decltype((TypeSet{ declval<Responder>().input } + ...));
-	using Outputs = decltype((TypeSet{ declval<Responder>().output } + ...));
-public:
+	using Inputs = decltype((TypeSet{ declval<InspectLambda<Responder>::Input>() } + ...));
+	using Outputs = decltype((TypeSet{ declval< InspectLambda<Responder>::Output>() } + ...));
 	using State = decltype(TypeSet{ declval<Inputs>() } + TypeSet{ declval<Outputs>() });
-	State state;
-	MadnessModel(Responder... responders) {};
-};
 
-template<class Input, class Output>
-class Responder {
+	typedef void(*Callback)(State&);
+	State _state;
+	unordered_map<std::type_index, vector<void*>> listeners;
+	unordered_set<std::type_index> unhandled_changes;
+	std::mutex change_handling_mutex;
+
+	static const int INFINITE_LOOP_LIMIT = 5;
+
+	void change_handler()
+	{
+		for (int i = 0; unhandled_changes.size() > 0 && i < INFINITE_LOOP_LIMIT; i++)
+		{
+			auto existing_unhandled_changes = unordered_set(unhandled_changes);
+			unhandled_changes.clear();
+
+
+			for_each(existing_unhandled_changes.begin(), existing_unhandled_changes.end(), [this](type_index unhandled_change) {
+				apply(
+					[this, unhandled_change](Responder... responder) {
+						return ((TupleHelper<InspectLambda<Responder>::Input>().hasIndex(unhandled_change) && submit(responder(TypeSet{ _state }))), ...);
+					}, responders);
+			});
+
+			unordered_set<void*> callbacks;
+			while (existing_unhandled_changes.size() > 0)
+			{
+				const auto it = --existing_unhandled_changes.end();
+				const auto element = *it;
+				const auto listeners = this->listeners[element];
+
+				for_each(listeners.begin(), listeners.end(), [&callbacks](auto listener) { callbacks.insert(listener); });
+
+				existing_unhandled_changes.erase(it);
+			}
+
+			for_each(
+				callbacks.begin(), callbacks.end(),
+				[this](void* callback) {
+					Callback callable = (Callback)callback;
+					callable(_state);
+				});
+		}
+	}
+
 public:
-	Responder(Output(*lambder)(Input)) {}
-	Input input;
-	Output output;
+	using ViewState = State;
+
+	tuple<Responder...> responders;
+	MadnessModel(Responder... responders) : responders{ tuple(responders...) } {};
+	template <TypeSetUtils::InTuple<State>... Member>
+	void listen(Callback callback)
+	{
+		(listeners[typeid(Member)].push_back((void*)callback), ...);
+	}
+	template <TypeSetUtils::InTuple<State>... Member>
+	bool submit(tuple<Member...> partialState)
+	{
+		_state = TypeSet{ _state }.merge(partialState);
+		(unhandled_changes.insert(typeid(Member)), ...);
+		std::unique_lock<std::mutex> lock(change_handling_mutex, std::try_to_lock);
+		if (lock.owns_lock())
+		{
+			change_handler();
+		}
+		return true;
+	}
 };
 
-struct Hi {
-	int what;
+struct LayerA1 {
+	int value;
 };
 
-struct Ho {
-
+struct LayerB1 {
+	int value;
 };
 
-struct Hum {
+struct LayerA2 {
+	int value;
+};
 
+struct LayerB2 {
+	int value;
+};
+
+struct LayerA3 {
+	int value;
 };
 
 namespace MadnessModelTester {
@@ -36,15 +118,18 @@ namespace MadnessModelTester {
 
 	void Test() {
 		auto model = MadnessModel(
-			Responder<tuple<Hum, double>, tuple<Hi>>(
-				[](auto input) {
-					return tuple(Hi{ 0 });
-				}),
-			Responder<tuple<int, double>, tuple<Ho, double>>(
-				[](auto input) {
-					return tuple(Ho{}, 0.0);
-				})
-			).state;
+			[](tuple<LayerA1> input) {
+				return tuple(LayerA2{ 12 });
+			},
+			[](tuple<LayerA2> input) {
+				return tuple(LayerA3{ 13 });
+			},
+			[](tuple<LayerB1> input) {
+				return tuple(LayerB2{ 22 });
+			}
+		);
+		decltype(model)::ViewState;
+		model.submit(tuple(LayerA1{ 11 }, LayerB1{ 21 }));
 		cout << "hi" << endl;
 	}
 }
